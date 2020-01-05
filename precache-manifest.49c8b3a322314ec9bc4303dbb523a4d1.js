10d16b91497b9fb470ab6caf715597eafc4e7663
adam001ae <adam001ae@gmail.com>
2020-01-05 19:28:17 +0100

    Updates

---
+++ precache-manifest.49c8b3a322314ec9bc4303dbb523a4d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ada40a8a709cd3fd83aa46d2e76e292",
+    "url": "/adam001ae/testujemy2/index.html"
+  },
+  {
+    "revision": "55f48008dfa5faa729dc",
+    "url": "/adam001ae/testujemy2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3b698eb2c7ff819de24c",
+    "url": "/adam001ae/testujemy2/static/js/2.edd1bec6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adam001ae/testujemy2/static/js/2.edd1bec6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55f48008dfa5faa729dc",
+    "url": "/adam001ae/testujemy2/static/js/main.ca8bd32a.chunk.js"
+  },
+  {
+    "revision": "ac406862d97f77c8bdde",
+    "url": "/adam001ae/testujemy2/static/js/runtime-main.972d9e2d.js"
+  }
+]);
